639b0fcdc5c6b1d1e95b2330ce849e15e96237b8
Zaytsev-Andrey <hendrey@yandex.ru>
2021-03-15 22:57:20 +0300

    Added homework4 lv3

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,6 +11,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.SQLException;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class Server {
@@ -18,18 +20,39 @@ public class Server {
     private Socket socket;
     private CopyOnWriteArrayList<ClientHandler> activeClients;
     private UserStorage userStorage;
+    private ExecutorService executorService;
 
     public Server() {
+        // Для запуска клиентских соединений в отдельных потоках использовал реализацию интерфейса ExecutorService
+        // создаваемую с помощью фабричного метода newCachedThreadPool() класса Executors. Данная реализация позволит
+        // мгновенно запускать новую задачу (используя существующий поток или создавая новый). Заметное увеличение
+        // прозводительности будет при интенсивном отключении и подключение новых клиентов в интервале 60 сек, когда
+        // освоодившиеся потоки не будут уничтожаться и смогут принимать новые задачи. Поскольку интенсивность подключий
+        // клиентов может быть не равномерной (в одно время суток достигать максимума, а в другое клиентских подключний
+        // может и не быть), реализация newCachedThreadPool позволит удалить не используемые потоки и освободить ресурсы.
+        // Большим недостатком такой реализации является фактически не ограниченное (Integer.MAX_VALUE) число возможных
+        // потоков, что может привести к перерасходу ресурсов.
+        //
+        // Фабричные методы класса Executors возвращают объекты класса ThreadPoolExecutor с фиксированными
+        // значениями конструктора, например для newCachedThreadPool: new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L,
+        // TimeUnit.SECONDS, new SynchronousQueue<Runnable>()) где вторым параметром передается максимально число потоков,
+        // а третьим параметром время простоя потока перед уничтожением. В случае обнаружения проблем в работе текущей
+        // реализации ExecutorService возможно создать свой ThreadPoolExecutor настроив его более точно (ограничив
+        // максимальное число потоков или увеличив время простоя потока перед уничтожением). При этом остальной код
+        // программы останется неизменным.
+        executorService = Executors.newCachedThreadPool();
+
         try {
             userStorage = new SQLiteStorage();
             activeClients = new CopyOnWriteArrayList();
             serverSocket = new ServerSocket(ParameterApp.PORT);
-            ConsoleLogger.serverIsRunning();
 
+            ConsoleLogger.serverIsRunning();
 
             while (true) {
                 socket = serverSocket.accept();
-                new ClientHandler(socket, this, userStorage);
+                ClientHandler newClient = new ClientHandler(socket, this, userStorage);
+                executorService.execute(newClient);
             }
 
         } catch (ClassNotFoundException | SQLException e) {
@@ -37,6 +60,8 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+            executorService.shutdown();
+
             if (serverSocket != null) {
                 try {
                     serverSocket.close();
