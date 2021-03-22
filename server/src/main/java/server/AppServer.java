d905cb81c292708ba6a6eff9d1fb52ab3e43838a
Zaytsev-Andrey <hendrey@yandex.ru>
2021-03-23 00:18:26 +0300

    Added homework6 lv3

--- server/src/main/java/server/AppServer.java
+++ server/src/main/java/server/AppServer.java
@@ -1,7 +1,22 @@
 package server;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class AppServer {
+    private static final Logger LOGGER = Logger.getLogger(AppServer.class.getName());
+
     public static void main(String[] args) {
+        LogManager manager = LogManager.getLogManager();
+        try {
+            manager.readConfiguration(new FileInputStream("server/logging.properties"));
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Error loading logger configuration", e);
+        }
+
         new Server();
     }
 }
--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -2,7 +2,6 @@ package server;
 
 import comands.ReasonAuthExceptions;
 import comands.SessionStatus;
-import log.ConsoleLogger;
 import messages.Message;
 import messages.MessageType;
 import server.exceptions.AuthExceptions;
@@ -16,8 +15,12 @@ import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClientHandler implements Runnable {
+    private static final Logger LOGGER = Logger.getLogger(ClientHandler.class.getName());
+
     private SessionStatus status;
     private Server server;
     private Socket socket;
@@ -35,12 +38,6 @@ public class ClientHandler implements Runnable {
         this.messageStorage = (MessageStorage) userStorage;
 
         status = SessionStatus.NOT_AUTH;
-
-//        new Thread(() -> {
-//
-//        }).start();
-
-
     }
 
     @Override
@@ -48,7 +45,8 @@ public class ClientHandler implements Runnable {
         try {
             out = new ObjectOutputStream(socket.getOutputStream());
             in = new ObjectInputStream(socket.getInputStream());
-            ConsoleLogger.clientConnectedToServer(socket.getInetAddress().toString());
+            String infoMsg = String.format("New connection from address %s", socket.getInetAddress().toString());
+            LOGGER.info(infoMsg);
             socket.setSoTimeout(120000);
 
             auth();
@@ -57,9 +55,11 @@ public class ClientHandler implements Runnable {
             Message msg = Message.createEndMessage();
             sendMsg(msg);
             status = SessionStatus.DISCONNECTED;
-            ConsoleLogger.authorizationTimedOut(socket.getInetAddress().toString());
+            String infoMsg = String.format("Connection timeout for address %s", socket.getInetAddress().toString());
+            LOGGER.info(infoMsg);
         } catch (IOException e) {
-            ConsoleLogger.clientInterruptedConnection(clientNick);
+            String errorMsg = String.format("Client connection \"%s\" is interrupted", clientNick);
+            LOGGER.log(Level.SEVERE, errorMsg, e);
         }
         finally {
             server.disconnectClient(this);
@@ -67,17 +67,17 @@ public class ClientHandler implements Runnable {
             try {
                 in.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.log(Level.SEVERE, "Output stream error", e);
             }
             try {
                 out.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.log(Level.SEVERE, "Input stream error", e);
             }
             try {
                 socket.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.log(Level.SEVERE, "Client socket error", e);
             }
         }
     }
@@ -85,13 +85,18 @@ public class ClientHandler implements Runnable {
     private void auth() throws IOException {
         Message msg;
 
+
         while (status == SessionStatus.NOT_AUTH) {
             try {
                 msg = (Message) in.readObject();
+                String commandMsg = String.format("Client %s sent command %s",
+                        socket.getInetAddress().toString(), msg.getMessageType().toString());
+                LOGGER.config(commandMsg);
 
                 if (msg.getMessageType() == MessageType.END) {
                     sendMsg(msg);
-                    ConsoleLogger.clientDisconnectedToServer(clientNick);
+                    String infoMsg = String.format("Client %s disconnected", socket.getInetAddress().toString());
+                    LOGGER.info(infoMsg);
                     status = SessionStatus.DISCONNECTED;
                 }
 
@@ -112,10 +117,14 @@ public class ClientHandler implements Runnable {
                         sendMsg(answerMsg);
                         server.connectClient(this);
                         socket.setSoTimeout(0);
-                        ConsoleLogger.clientPassedAuth(clientNick, socket.getInetAddress().toString());
+                        String infoMsg = String.format("Client %s connected", clientNick);
+                        LOGGER.info(infoMsg);
                     } catch (AuthExceptions e) {
                         answerMsg = Message.createAuthFailMessage(e.getReason());
                         sendMsg(answerMsg);
+                        String errorMsg = String.format("Authentication error from address %s. %s",
+                                socket.getInetAddress().toString(), e.getReason());
+                        LOGGER.warning(errorMsg);
                     }
                 }
 
@@ -126,13 +135,17 @@ public class ClientHandler implements Runnable {
                         answerMsg = Message.createRegOkMessage();
                     } catch (AuthExceptions e) {
                         answerMsg = Message.createRegFailMessage(e.getReason());
+                        String errorMsg = String.format("Registration error with login \"%s\" and nickname \"%s\". %s",
+                                msg.getLogin(), msg.getNick(), e.getReason());
+                        LOGGER.warning(errorMsg);
                     }
 
                     sendMsg(answerMsg);
                 }
 
             } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+                String errorMsg = String.format("Invalid message format from client %s", clientNick);
+                LOGGER.log(Level.WARNING, errorMsg, e);
             }
         }
     }
@@ -143,10 +156,14 @@ public class ClientHandler implements Runnable {
         while (status == SessionStatus.CONNECTED) {
             try {
                 msg = (Message) in.readObject();
+                String commandMsg = String.format("Client %s sent command %s",
+                        clientNick, msg.getMessageType().toString());
+                LOGGER.config(commandMsg);
 
                 if (msg.getMessageType() == MessageType.END) {
                     sendMsg(msg);
-                    ConsoleLogger.clientDisconnectedToServer(clientNick);
+                    String infoMsg = String.format("Client %s disconnected", clientNick);
+                    LOGGER.info(infoMsg);
                     status = SessionStatus.DISCONNECTED;
                 }
 
@@ -161,6 +178,9 @@ public class ClientHandler implements Runnable {
                     } catch (AuthExceptions e) {
                         answerMsg = Message.createChangeNickFailMessage(e.getReason());
                         sendMsg(answerMsg);
+                        String errorMsg = String.format("Error changing nickname \"%s\" to \"%s\". %s",
+                                clientNick, msg.getNewNick(), e.getReason());
+                        LOGGER.warning(errorMsg);
                     }
                 }
 
@@ -170,7 +190,8 @@ public class ClientHandler implements Runnable {
                         Message answerMsg = Message.createTextListMessage(messageList);
                         sendMsg(answerMsg);
                     } catch (SQLException e) {
-                        e.printStackTrace();
+                        String errorMsg = String.format("Error getting message list for client \"%s\"", clientNick);
+                        LOGGER.log(Level.WARNING, errorMsg, e);
                     }
                 }
 
@@ -178,10 +199,9 @@ public class ClientHandler implements Runnable {
                     try {
                         messageStorage.addMessage(clientNick, msg.getRecipient(), msg.getText());
                         server.broadcastMessage(msg);
-                    } catch (IllegalArgumentException e) {
-                        e.printStackTrace();
                     } catch (SQLException e) {
-                        e.printStackTrace();
+                        String errorMsg = String.format("Error adding message \"%s\" to database", msg.getText());
+                        LOGGER.log(Level.WARNING, errorMsg, e);
                     }
                 }
 
@@ -189,14 +209,14 @@ public class ClientHandler implements Runnable {
                     try {
                         messageStorage.addMessage(clientNick, msg.getRecipient(), msg.getText());
                         server.privateMessage(msg, this);
-                    } catch (IllegalArgumentException e) {
-                        e.printStackTrace();
                     } catch (SQLException e) {
-                        e.printStackTrace();
+                        String errorMsg = String.format("Error adding message \"%s\" to database", msg.getText());
+                        LOGGER.log(Level.WARNING, errorMsg, e);
                     }
                 }
             } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+                String errorMsg = String.format("Invalid message format from client %s", clientNick);
+                LOGGER.log(Level.WARNING, errorMsg, e);
             }
         }
     }
@@ -206,7 +226,8 @@ public class ClientHandler implements Runnable {
             out.writeObject(msg);
             out.flush();
         } catch (IOException e) {
-            e.printStackTrace();
+            String errorMsg = String.format("Error sending message \"%s\" to client \"%s\"", msg.getText(), clientNick);
+            LOGGER.log(Level.WARNING, errorMsg, e);
         }
     }
 
